08b7c66cd933e61090cdf7b72b346b2a976350b5
Lee <lishilei0523@163.com>
2021-04-19 16:17:26 +0800

    添加示例项目；

---
+++ samples/SD.FileSystem.WindowsClient/Program.cs
@@ -0,0 +1,107 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace SD.FileSystem.WindowsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //上传单文件
+            //UploadFile();
+
+            //上传多文件
+            //UploadFiles();
+
+            Console.ReadKey();
+        }
+
+        static void UploadFile()
+        {
+            string url = "http://localhost:4987/Load/UploadFile";
+
+            RestRequest request = new RestRequest(Method.POST);
+            request.AddParameter("use", "用途");
+            request.AddParameter("description", "描述");
+
+            string filePath = CreateTestFile();
+            string fileName = Path.GetFileName(filePath);
+            byte[] fileBytes;
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fileBytes = new byte[stream.Length];
+                stream.Read(fileBytes, 0, fileBytes.Length);
+            }
+            request.AddFileBytes("formFile", fileBytes, fileName);
+
+            RestClient restClient = new RestClient(url);
+            IRestResponse response = restClient.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("上传成功！\n" + response.Content);
+            }
+            else
+            {
+                Console.WriteLine("上传失败！\n" + response.Content);
+            }
+
+            File.Delete(filePath);
+        }
+
+        static void UploadFiles()
+        {
+            string url = "http://localhost:4987/Load/UploadFiles";
+
+            RestRequest request = new RestRequest(Method.POST);
+            request.AddParameter("use", "用途");
+            request.AddParameter("description", "描述");
+
+            IList<string> filePaths = new List<string>();
+            for (int index = 0; index < 3; index++)
+            {
+                string filePath = CreateTestFile();
+                string fileName = Path.GetFileName(filePath);
+
+                byte[] fileBytes;
+                using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    fileBytes = new byte[stream.Length];
+                    stream.Read(fileBytes, 0, fileBytes.Length);
+                }
+
+                request.AddFileBytes("formFiles", fileBytes, fileName);
+                filePaths.Add(filePath);
+            }
+
+            RestClient restClient = new RestClient(url);
+            IRestResponse response = restClient.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("上传成功！\n" + response.Content);
+            }
+            else
+            {
+                Console.WriteLine("上传失败！\n" + response.Content);
+            }
+
+            foreach (string filePath in filePaths)
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        static string CreateTestFile()
+        {
+            string fileName = Guid.NewGuid().ToString();
+            string text = "ReadMe";
+            string path = $@"D:\{fileName}.txt";
+
+            File.WriteAllText(path, text);
+
+            return path;
+        }
+    }
+}
