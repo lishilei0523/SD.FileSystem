14bacdd433f6628d54af25c2e8e9f9dd81d6de15
Lee <lishilei0523@163.com>
2022-01-29 13:02:54 +0800

    临时提交；

--- src/SD.FileSystem.IAppService/DTOs/Inputs/DownloadRequest.cs
+++ src/SD.FileSystem.IAppService/DTOs/Inputs/DownloadRequest.cs
@@ -9,6 +9,25 @@ namespace SD.FileSystem.IAppService.DTOs.Inputs
     [MessageContract]
     public class DownloadRequest
     {
+        #region 构造器
+
+        /// <summary>
+        /// 无参构造器
+        /// </summary>
+        public DownloadRequest() { }
+
+        /// <summary>
+        /// 创建下载请求构造器
+        /// </summary>
+        /// <param name="fileId">文件Id</param>
+        public DownloadRequest(Guid fileId)
+            : this()
+        {
+            this.FileId = fileId;
+        }
+
+        #endregion
+
         #region 文件Id —— Guid FileId
         /// <summary>
         /// 文件Id
